a000fc6fc2f9a2305319a87828fe6beb5f033013
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-08-13 02:55:30 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,13 +2,39 @@
 """BaseModel"""
 
 
-class User(BaseModel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Additional attributes specific to User class
+import uuid
+from datetime import datetime
 
-
-class Post(BaseModel):
+class BaseModel:
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Additional attributes specific to Post class
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+

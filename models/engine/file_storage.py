c5342c403a6f2361e2c600c865705b7a5eabc256
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-08-13 14:06:24 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,8 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances"""
+    """Serializes instances to a JSON file
+        and deserializes JSON file to instances"""
 
     __file_path = "file.json"
     __objects = {}

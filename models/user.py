35bbbd3edeb73d1bd5128d0d15b71ae47d085714
Ayomide <ilesanmibry@gmail.com>
2023-08-12 20:00:47 -0700

    commit

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""User class"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    "Defines user class"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""

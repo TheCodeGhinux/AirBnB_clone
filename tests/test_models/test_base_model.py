2013c6995383ac816d184fe6d927fa659cbd6c55
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-08-13 17:31:42 +0100

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python3
-"""Test for BaseModel"""
-
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 import models
 
+
 class TestBaseModel(unittest.TestCase):
-    """Definea the Test class"""
+    """Test cases for the BaseModel class."""
 
     def setUp(self):
         """Set up a new instance of BaseModel for each test."""
@@ -20,41 +18,43 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(hasattr(self.base_model, 'updated_at'))
 
     def test_id_is_string(self):
-        """Test if the 'id' attribute is a string."""
+        """Test if 'id' attribute is a string."""
         self.assertIsInstance(self.base_model.id, str)
 
     def test_created_at_is_datetime(self):
-        """Test if 'created_at' attribute is a datetime object."""
+        """Test if 'created_at' is a datetime object."""
         self.assertIsInstance(self.base_model.created_at, datetime)
 
     def test_updated_at_is_datetime(self):
-        """Test if 'updated_at' attribute is a datetime object."""
+        """Test if 'updated_at' is a datetime object."""
         self.assertIsInstance(self.base_model.updated_at, datetime)
 
     def test_save(self):
-        """Test if 'save' method updates the 'updated_at' attribute."""
+        """Test if 'save' updates the 'updated_at' attribute."""
         prev_updated_at = self.base_model.updated_at
         self.base_model.save()
         self.assertNotEqual(prev_updated_at, self.base_model.updated_at)
 
     def test_to_dict(self):
-        """Test if 'to_dict' method returns a valid dictionary."""
-        self.base_model_dict = self.base_model.to_dict()
-        self.assertIsInstance(self.base_model_dict, dict)
-        self.assertEqual(self.base_model_dict['__class__'], 'BaseModel')
-        self.assertIsInstance(self.base_model_dict['created_at'], str)
-        self.assertIsInstance(self.base_model_dict['updated_at'], str)
+        """Test if 'to_dict' returns a valid dictionary."""
+        base_model_dict = self.base_model.to_dict()
+        self.assertIsInstance(base_model_dict, dict)
+        self.assertEqual(base_model_dict['__class__'], 'BaseModel')
+        self.assertIsInstance(base_model_dict['created_at'], str)
+        self.assertIsInstance(base_model_dict['updated_at'], str)
 
     def test_str(self):
-        """Test if the '__str__' method returns the expected string representation."""
-        expected_str = "[BaseModel] ({}) {}".format(self.base_model.id, self.base_model.__dict__)
+        """Test if '__str__' returns the expected string."""
+        expected_str = "[BaseModel] ({}) {}".format(
+            self.base_model.id, self.base_model.__dict__)
         self.assertEqual(str(self.base_model), expected_str)
 
     def test_all(self):
-        """Test if 'all' method returns a dictionary containing instances."""
+        """Test if 'all' returns a dictionary containing instances."""
         all_instances = BaseModel.all()
         self.assertIsInstance(all_instances, dict)
         self.assertIn(self.base_model, all_instances.values())
+
 
 if __name__ == '__main__':
     unittest.main()

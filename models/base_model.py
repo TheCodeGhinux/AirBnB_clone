d5e27d6e1256b24b8ed5c2c8cc15e74fb14c1a14
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-08-13 16:47:01 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -45,6 +45,10 @@ class BaseModel:
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
 
+    def __str__(self):
+        """Returns a string representation of the BaseModel instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
     @classmethod
     def all(cls):
         """Returns a dictionary of all instances of BaseModel."""

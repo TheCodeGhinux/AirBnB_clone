51a049a511d9bbb640b6a166a4ef5b5731240564
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-08-14 01:37:11 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""User class"""
+"""User Class"""
 
 from models.base_model import BaseModel
 
-
 class User(BaseModel):
-    "Defines user class"
+    """Defines class for User"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize the user"""
+        """
+        Initialize a new User.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
         super().__init__(*args, **kwargs)
         self.email = ""
         self.password = ""
         self.first_name = ""
         self.last_name = ""
+
+    def __str__(self):
+        """Returns a string representation of the User instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)

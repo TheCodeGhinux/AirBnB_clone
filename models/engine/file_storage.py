cc9294ce0fb321f49655bec26ec3d1da8bb4b055
Ayomide Ilesanmi <65258939+TheCodeGhinux@users.noreply.github.com>
2023-08-14 01:35:37 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,7 @@
 import json
 from os.path import exists
 from models.base_model import BaseModel
-
+from models.user import User
 
 class FileStorage:
     """Represent an abstracted storage engine"""

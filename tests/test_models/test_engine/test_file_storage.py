3c80abeedb6e79efa91e48711a7a723cd6fe8631
Ayomide <ilesanmibry@gmail.com>
2023-08-15 18:37:55 +0100

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,16 +8,6 @@ from models.user import User
 from models.engine.file_storage import FileStorage
 
 
-#!/usr/bin/python3
-"""Unit test for FileStorage"""
-
-import unittest
-import os
-from models.base_model import BaseModel
-from models.user import User
-from models.engine.file_storage import FileStorage
-
-
 class TestFileStorage(unittest.TestCase):
     """
     Defines class for file storage unit test.
@@ -33,12 +23,20 @@ class TestFileStorage(unittest.TestCase):
         except FileNotFoundError:
             pass
 
+    def test_save(self):
+        obj1 = BaseModel()
+        obj2 = User()
+        obj1.save()
+        obj2.save()
+
+        self.storage.save()
+
+        self.assertTrue(os.path.exists(self.storage._FileStorage__file_path))
+
     def test_reload(self):
-        # Create an empty file before testing reload
         with open(self.storage._FileStorage__file_path, 'w') as f:
             f.write('{}')
 
-        # Perform the reload
         self.storage.reload()
 
         all_objs = self.storage.all()

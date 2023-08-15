d8b76ab8f678de88d44f26d29f240e2e8321e14a
Ayomide <ilesanmibry@gmail.com>
2023-08-15 17:55:58 +0100

    Unit test for file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Unit test for Filestorage"""
+import unittest
+from models.base_model import BaseModel
+from models.user import User
+from models import storage
+from models.engine.file_storage import FileStorage
+import os
+import json
+
+class TestFileStorage(unittest.TestCase):
+    """
+    Defines class for file storage
+    unit test.
+    Tes the methods in file storage
+    """
+
+    def setUp(self):
+        self.storage = FileStorage()
+
+    def tearDown(self):
+        try:
+            os.remove(self.storage._FileStorage__file_path)
+        except:
+            pass
+
+    def test_all_empty(self):
+        all_objs = self.storage.all()
+        self.assertEqual(type(all_objs), dict)
+        self.assertEqual(len(all_objs), 0)
+
+    def test_all_non_empty(self):
+        obj1 = BaseModel()
+        obj2 = User()
+        obj1.save()
+        obj2.save()
+
+        all_objs = self.storage.all()
+        self.assertEqual(len(all_objs), 2)
+        self.assertIn("BaseModel." + obj1.id, all_objs)
+        self.assertIn("User." + obj2.id, all_objs)
+
+if __name__ == '__main__':
+    unittest.main()

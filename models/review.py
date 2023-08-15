537de54b409409fedcf59b218097c80c8e6c0ef4
Ayomide <ilesanmibry@gmail.com>
2023-08-15 18:07:48 +0100

    updated, pycodestyle implementation

--- models/review.py
+++ models/review.py
@@ -2,6 +2,7 @@
 
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
     """Defines class for Review
 
@@ -14,4 +15,3 @@ class Review(BaseModel):
     place_id = ""
     user_id = ""
     text = ""
-

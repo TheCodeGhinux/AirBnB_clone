62f7c4f7a49e279139ca7f2f82f7a0e86dfdce08
Ayomide <ilesanmibry@gmail.com>
2023-08-15 18:33:01 +0100

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -20,27 +20,19 @@ class TestFileStorage(unittest.TestCase):
     def tearDown(self):
         try:
             os.remove(self.storage._FileStorage__file_path)
-        except FileNotFoundError:
+        except:
             pass
 
-    def test_all_empty(self):
-        all_objs = self.storage.all()
-        self.assertEqual(type(all_objs), dict)
-        self.assertEqual(len(all_objs), 0)
+    def test_reload(self):
+        # Create an empty file before testing reload
+        with open(self.storage._FileStorage__file_path, 'w') as f:
+            f.write('{}')
 
-    def test_all_non_empty(self):
-        obj1 = BaseModel()
-        obj2 = User()
-        obj1.save()
-        obj2.save()
-
-        self.storage.save()
+        # Perform the reload
         self.storage.reload()
 
         all_objs = self.storage.all()
-        self.assertEqual(len(all_objs), 2)
-        self.assertIn("BaseModel." + obj1.id, all_objs)
-        self.assertIn("User." + obj2.id, all_objs)
+        self.assertEqual(len(all_objs), 0)
 
 
 if __name__ == '__main__':

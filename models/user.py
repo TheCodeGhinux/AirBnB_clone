89df47d13bd9f24db377174edb080f485080b90f
Ayomide <ilesanmibry@gmail.com>
2023-08-15 19:01:08 +0100

    updated user

--- models/user.py
+++ models/user.py
@@ -5,23 +5,16 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Defines class for User"""
+    """
+    Defines class for User
+    Attrs:
+        email (string): User's email.
+        first_name (string): User's first_name.
+        last_name (string): User's last_name.
+        password (string): User's password.
+    """
 
-    def __init__(self, *args, **kwargs):
-        """
-        Initialize a new User.
-
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
-
-    def __str__(self):
-        """Returns a string representation of the User instance."""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
